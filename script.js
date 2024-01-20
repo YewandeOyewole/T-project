9d1630f476bf65cb2b86a21f57bb813683429569
YewandeOyewole <wandeoyewo@gmail.com>
2024-01-20 10:34:05 +0100

    final project

--- script.js
+++ script.js
@@ -25,4 +25,31 @@ const swiper = new Swiper('.swiper', {
       el: '.swiper-scrollbar',
     },
   });
+
+  // cowndown control
+     
+
+       const countdown = () => {
+        const countDate = new Date("Febuary 1,2024 00:00:00").getTime()
+        const now = new Date().getTime();
+        const gap = countDate - now;
+        // console.log(countDate);
+
+        const second = 1000;
+        const minutes = second * 60;
+        const hour = minutes * 60;
+        const day = hour * 24;
+
+        const textDay = Math.floor(gap / day);
+        const textHour = Math.floor((gap % day) /hour);
+        const textMinutes = Math.floor((gap % day) / minutes);
+        const textSecond = Math.floor((gap % day) / second);
+        // console.log(textDay)
+           document.querySelector(".day").innerText = textDay;
+        document.querySelector(".hour").innerText = textHour;
+        document.querySelector(".minutes").innerText = textMinutes;
+        document.querySelector(".second").innerText = textSecond;
+  };
+
+  countdown();
   
